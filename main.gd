ca8e59f0d32468786a582efe3cc891725797355b
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-09-18 23:20:23 +0530

    pilot

--- main.gd
+++ main.gd
@@ -1,8 +1,11 @@
-extends Node2D
+extends Node
 
 
 
-func _on_quit_pressed() -> void:
+
+func _on_pressed() -> void:
 	get_tree().quit()
 
 
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://World.tscn")
